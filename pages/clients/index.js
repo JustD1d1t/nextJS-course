f060ab9a894ac4ca02d8729459d2bc3874a4296e
JustD1d1t <starzyk.dawid.0826@gmail.com>
2022-05-12 14:01:02 +0200

    routing

---
+++ pages/clients/index.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+const ClientsPage = () => {
+  const clients = [
+    { id: "dawid", name: "Dawid" },
+    { id: "test", name: "Test" },
+  ];
+  return (
+    <div>
+      <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: {
+                  id: client.id,
+                },
+              }}
+            >
+              {client.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ClientsPage;
